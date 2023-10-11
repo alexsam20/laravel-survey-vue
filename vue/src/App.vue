4dcb2c763f56af0a1438a602f271748b0b9fe177
Alex Sam <alsam3637@gmail.com>
2023-10-11 17:33:22 -0400

    Install vite. Create store and index.js file.

---
+++ vue/src/App.vue
@@ -0,0 +1,58 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import {mapState} from 'vuex';
+
+export default {
+  components: {
+    HelloWorld
+  },
+  computed: {
+    ...mapState(['user'])
+  }
+}
+</script>
+
+<template>
+  <pre>{{user}}</pre>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
